d10255dd201e420468e2902880cd451efdbc7b39
Kris Winer <kriswiner@users.noreply.github.com>
2019-11-11 17:19:48 -0800

    Add files via upload

---
+++ PAW3902/PAW3902.h
@@ -0,0 +1,56 @@
+/* PAW3902 Optical Flow Sensor
+ * Copyright (c) 2019 Tlera Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef __PAW3902_H
+#define __PAW3902_H
+
+#include "Arduino.h"
+
+#include <stdint.h>
+
+#define bright        1
+#define lowlight      2
+#define superlowlight 3
+
+class PAW3902 {
+public:
+  PAW3902(uint8_t cspin);
+  boolean begin(void);
+  uint8_t status();
+  void readMotionCount(int16_t *deltaX, int16_t *deltaY, uint8_t *SQUAL, uint16_t *Shutter);
+  void readBurstMode(uint8_t * dataArray); 
+  boolean checkID();
+  void setMode(uint8_t mode);
+  void reset();
+  void shutdown();
+
+private:
+  uint8_t _cs;
+  void writeByte(uint8_t reg, uint8_t value);
+  uint8_t readByte(uint8_t reg);
+  void initBright(void);
+  void initLowLight(void);
+  void initSuperLowLight(void);
+};
+
+#endif //__PAW3902_H
+
---
+++ PAW3902/PAW3902.ino
@@ -0,0 +1,95 @@
+/* 11/11/2019 Copyright Tlera Corporation
+ *  
+ *  Created by Kris Winer
+ *  
+ This sketch is to operate the 
+ 
+ The sketch uses default SPI pins on the Dragonfly development board.
+
+ Sketch modeled after Bitcraze PMW3901 library-https://github.com/bitcraze/PMW3901
+
+ Library may be used freely and without limit with attribution.
+ 
+  */
+#include <SPI.h>
+#include "PAW3902.h"
+
+// Pin definitions
+#define myLed  26  // blue led on Dragonfly
+#define CSPIN  10  // default chip select for SPI
+#define EN     31  // board enable is pulled up, set to TTL LOW to disable
+#define RST    21  // PAM3902 reset
+#define MOT    30  // use as data ready interrupt
+
+uint8_t mode = lowlight; // mode choices are bright, lowlight (default), superlowlight
+int16_t deltaX, deltaY;
+bool motionDetect = false, alarmFlag = false;
+uint8_t status;
+uint8_t dataArray[12], SQUAL;
+uint16_t Shutter;
+
+PAW3902 opticalFlow(CSPIN); // Instantiate PAW3902
+
+void setup() {
+  Serial.begin(115200);
+  delay(4000);
+  Serial.println("Serial enabled!");
+
+  // Configure led
+  pinMode(myLed, OUTPUT);
+  digitalWrite(myLed, HIGH); // start with led off since active LOW on Dragonfly
+
+  pinMode(MOT, INPUT); // data ready interrupt
+
+  // Configure SPI Flash chip select
+  pinMode(CSPIN, OUTPUT);
+  digitalWrite(CSPIN, HIGH);
+
+  SPI.begin(); // initiate SPI 
+  delay(1000);
+
+  digitalWrite(myLed, LOW);
+
+  opticalFlow.begin();  // Prepare SPI port and restart device
+  opticalFlow.reset();  // Reset device
+  
+  // Check device ID as a test of SPI communications
+  if (!opticalFlow.checkID()) {
+  Serial.println("Initialization of the opticalFlow sensor failed");
+  while(1) { }
+  
+  opticalFlow.setMode(mode); // set device mode
+
+  digitalWrite(myLed, HIGH);
+  }
+
+  attachInterrupt(MOT, myIntHandler, FALLING); // active LOW 
+  status = opticalFlow.status();  // clear interrupt before entering main loop
+  /* end of setup */
+
+}
+
+void loop() {
+  
+  if(motionDetect)
+  {
+   motionDetect = false;
+   
+   status = opticalFlow.status();
+   opticalFlow.readMotionCount(&deltaX, &deltaY, &SQUAL, &Shutter); 
+//   opticalFlow.readBurstMode(dataArray);
+//   deltaX = ((int16_t)dataArray[3] << 8) | dataArray[2];
+//   deltaY = ((int16_t)dataArray[5] << 8) | dataArray[4];
+//   SQUAL = dataArray[6];
+//   Shutter = (uint16_t)((dataArray[11] & 0x1F) << 8) | dataArray[10];
+   Serial.print("X: ");Serial.print(deltaX);Serial.print(", Y: ");Serial.println(deltaY);
+   Serial.print("SQUAL: 0x");Serial.print(SQUAL, HEX);Serial.print(", Shutter: 0x");Serial.println(Shutter & 0x1FFF, HEX);
+  }
+  delay(100);
+  }
+
+
+void myIntHandler()
+{
+  motionDetect = true;
+}
